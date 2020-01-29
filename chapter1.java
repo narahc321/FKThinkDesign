baea5ae7a4579bd06dd340b9c4ec459888b64d6c
Narahc321 <tanguturucharan@gmail.com>
2020-01-29 23:07:03 +0530

    Changes to be committed: 	new file:   chapter1.java
    There are no codes in chapter1

---
+++ chapter1.java
@@ -0,0 +1,17 @@
+/*
+javac chapter1.java -d ClassFiles
+java -cp  ClassFiles/ PracticalObjectOrientedDesign.chapter1
+*/
+
+
+
+// No Codes in this chapter
+package PracticalObjectOrientedDesign;
+
+public class chapter1{
+	public static void main(String[] args){
+
+	}
+}
+
+
